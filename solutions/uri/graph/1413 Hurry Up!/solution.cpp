3c555e07437c5891bc630be2329d60d26398bb67
camilo palacios <camilopalacios772@gmail.com>
2020-09-18 13:16:27 -0500

    flows, dp, and greedy problems

--- solutions/uri/graph/1413 Hurry Up!/solution.cpp
+++ solutions/uri/graph/1413 Hurry Up!/solution.cpp
@@ -4,7 +4,7 @@
 #define sd second
 using namespace std;
 
-const int INF = 1e9;
+const double INF = 1e9;
 
 struct MCMF {
     struct Edge {
@@ -12,25 +12,27 @@ struct MCMF {
         double cost;
         Edge(int to, int cap, double cost): to(to), cap(cap), cost(cost) {}
     };
-    int sz;
+    int n, m;
     vector<Edge> edges;
     vector<vector<int>> adj;
-    MCMF(int n): sz(n) {
-        adj.resize(sz);
+    MCMF(int n): n(n) {
+        adj.resize(n);
+        m = 0;
     }
     void add_edge(int u, int v, int cap, double cost) {
         edges.emplace_back(v, cap, cost);
-        adj[u].emplace_back(edges.size() - 1);
         edges.emplace_back(u, 0, -cost);
-        adj[v].emplace_back(edges.size() - 1);
+        adj[u].emplace_back(m);
+        adj[v].emplace_back(m + 1);
+        m += 2;
     }
-    int mcmf(int s, int t) {
-        int f = 0;
-        double c = 0;
+    double mcmf(int s, int t) {
+        int flow = 0;
+        double cost = 0;
         while(true) {
-            vector<double> dist(sz, INF);
-            set<pair<double,int>> q;
-            vector<int> p(sz, -1);
+            vector<double> dist(n, INF);
+            set<pair<double, int>> q;
+            vector<int> p(n, -1);
             dist[s] = 0;
             q.insert({0, s});
             while(!q.empty()) {     // dijkstra
@@ -39,7 +41,8 @@ struct MCMF {
                 q.erase(q.begin());
                 for(auto & id : adj[u]) {
                     int v = edges[id].to;
-                    if(edges[id].cap - edges[id].flow > 0 && dist[u] + edges[id].cost < dist[v]) {
+                    if(edges[id].cap - edges[id].flow < 1) continue;
+                    if(dist[u] + edges[id].cost < dist[v]) {
                         q.erase({dist[v], v});
                         dist[v] = dist[u] + edges[id].cost;
                         p[v] = id;
@@ -48,20 +51,20 @@ struct MCMF {
                 }
             }
             if(p[t] == -1) break;
-            int flow = 0;
+            int f = (int) INF;
             for(int i = t; i != s; i = edges[p[i] ^ 1].to) {
                 int id = p[i];
-                flow = min(flow, edges[id].cap - edges[id].flow);
+                f = min(f, edges[id].cap - edges[id].flow);
             }
-            f += flow;
-            c += f * dist[t];
+            flow += f;
+            cost += f * dist[t];
             for(int i = t; i != s; i = edges[p[i] ^ 1].to) {
                 int id = p[i];
-                edges[id].flow -= flow;
-                edges[id ^ 1].flow += flow;
+                edges[id].flow += f;
+                edges[id ^ 1].flow -= f;
             }
         }
-        return c;
+        return cost;
     }
 };
 
@@ -69,35 +72,37 @@ const int MXN = 110;
 double x[MXN], y[MXN], speed[MXN];
 
 double dist(double x1, double y1, double x2, double y2) {
-    double dx = 1.0*x1 - x2;
-    double dy = 1.0*y1 - y2;
+    double dx = x1 - x2;
+    double dy = y1 - y2;
     return sqrt(dx*dx + dy*dy);
 }
 
 int main() {
-    // ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cout << fixed << setprecision(1);
     int n, m;
-    while(cin >> n >> m && (n || m)) {
+    while(cin >> n >> m) {
+        if(n == 0 && m == 0) break;
         for(int i = 0; i < n; i++) {
             cin >> x[i] >> y[i] >> speed[i];
         }
         int s = n + m, t = n + m + 1;
-        MCMF g(t + 1);
+        MCMF mcmf(t + 1);
         for(int i = 0; i < m; i++) {
             double tx, ty; int c;
             cin >> tx >> ty;
             while(cin >> c && c) {
                 c--;
-                g.add_edge(c, n + i, 1, dist(x[c], y[c], tx, ty) * speed[c]);
+                mcmf.add_edge(c, n + i, 1, dist(x[c], y[c], tx, ty) / speed[c]);
             }
         }
         for(int i = 0; i < n; i++) {
-            g.add_edge(s, i, 1, 0);
+            mcmf.add_edge(s, i, 1, 0);
         }
         for(int i = 0; i < m; i++) {
-            g.add_edge(n + i, t, 1, 0);
+            mcmf.add_edge(n + i, t, 1, 0);
         }
-        cout << g.mcmf(s, t) << endl;
+        cout << mcmf.mcmf(s, t) << endl;
     }
     return 0;
 }
